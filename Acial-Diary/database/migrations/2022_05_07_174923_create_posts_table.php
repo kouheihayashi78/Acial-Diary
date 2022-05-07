250d72561ab773f031c1eb6bda91ecf7a8b72bec
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2022-05-07 22:08:37 +0900

    テーブル追加

--- Acial-Diary/database/migrations/2022_05_07_174923_create_posts_table.php
+++ Acial-Diary/database/migrations/2022_05_07_174923_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('body');
+            $table->string('img')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
-            $table->tinyInteger('type')->default(1);
             $table->tinyInteger('active')->default(1);
-            $table->string('icon')->nullable();
+            $table->tinyInteger('publish')->default(1);
         });
     }
 
@@ -34,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
