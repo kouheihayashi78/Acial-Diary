fdc44b36ba07e81ebdb57a7feda44de393520a30
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2022-05-22 13:51:54 +0900

    Admin.Model作成

--- Acial-Diary/app/Models/Admin.php
+++ Acial-Diary/app/Models/Admin.php
@@ -2,27 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class Admin extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    use HasFactory;
     protected $fillable = [
         'name',
         'email',
         'password',
-        'active',
-        'icon'
     ];
 
     /**
@@ -43,9 +32,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function posts()
-    {
-        return $this->hasMany('App\Models\Post');
-    }
 }
--- Acial-Diary/database/migrations/2022_05_22_134731_create_admins_table.php
+++ Acial-Diary/database/migrations/2022_05_22_134731_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id()->comment('登録者ID');
-            $table->string('name', 50)->comment('登録者名');
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id()->comment('管理者ID');
+            $table->string('name', 50)->comment('管理者名');
             $table->string('email', 100)->unique()->comment('メールアドレス');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 100)->comment('パスワード');
             $table->rememberToken()->comment('パスワード再発行トークン');
             $table->timestamps();
-            $table->tinyInteger('active')->default(1)->comment('利用可能フラグ');
-            $table->string('icon')->nullable()->comment('アイコン');
         });
     }
 
@@ -33,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 }
